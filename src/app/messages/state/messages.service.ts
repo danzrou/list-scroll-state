d4c62c3f6a524e6bf1f85296759308cb39bd0e43
Dan Roujinsky <danr@siemplify.co>
2020-05-11 12:01:20 +0300

    update markAllRead

--- src/app/messages/state/messages.service.ts
+++ src/app/messages/state/messages.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-import { transaction } from '@datorama/akita';
 import { createNewMessage } from './data';
 import { Message, MessageType } from './message.model';
-import { MessagesQuery } from './messages.query';
 import { MessagesStore } from './messages.store';
 
 @Injectable({ providedIn: 'root' })
 export class MessagesService {
-  constructor(private store: MessagesStore, private query: MessagesQuery) {}
+  constructor(private store: MessagesStore) {}
 
   toggleFilter(type: MessageType) {
     this.store.toggleFilter(type);
@@ -15,7 +13,6 @@ export class MessagesService {
 
   addMessage() {
     const msg = createNewMessage();
-    console.log(`New msg`, msg);
     this.store.upsertMany([msg]);
   }
 
@@ -23,13 +20,7 @@ export class MessagesService {
     this.store.update(id, { isNew: false });
   }
 
-  @transaction()
   markAllRead() {
-    const ids = this.query
-      .getAll({ filterBy: (msg) => msg.isNew === true })
-      .map((msg) => msg.id);
-    for (const id of ids) {
-      this.store.update(id, { isNew: false });
-    }
+    this.store.update((entity) => entity.isNew === true, { isNew: false });
   }
 }
